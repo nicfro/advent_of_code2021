15a51bcb9d97dd595464f5fc05903b6aa45b37af
nicfro <nicolai.frost@gmail.com>
2021-12-01 23:17:17 +0100

    clean up

--- day1/solution.py
+++ day1/solution.py
@@ -1,16 +1,18 @@
 with open('input.txt') as f:
     lines = f.readlines()
 
+lines = [int(i.strip()) for i in lines]
+
 '''
 Part 1
 count the number of times a depth measurement increases from the previous measurement.
 
 O(n) solution
 '''
-current = int(lines[0].strip())
+current = lines[0]
 counter = 0
 for i in range(1, len(lines)):
-    new_line = int(lines[i].strip())
+    new_line = lines[i]
     if current < new_line:
         counter += 1
     current = new_line
@@ -25,16 +27,15 @@ O(n) solution - for any sliding window, no sum.
 '''
 
 last_in_sum_idx = 0
-last_in_sum = int(lines[last_in_sum_idx].strip())
-current_sum = sum([int(x.strip()) for x in lines[:3]])
+last_in_sum = lines[last_in_sum_idx]
+current_sum = sum(lines[:3])
 counter = 0
 for i in range(3, len(lines)):
-    next_in_sum = int(lines[i].strip())
-    new_sum = (current_sum - last_in_sum) + next_in_sum
+    new_sum = (current_sum - last_in_sum) + lines[i]
     if current_sum < new_sum:
         counter += 1
     current_sum = new_sum
     last_in_sum_idx += 1
-    last_in_sum = int(lines[last_in_sum_idx].strip())
+    last_in_sum = lines[last_in_sum_idx]
 
 print(counter)
